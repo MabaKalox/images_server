3e49bc6697530684cf8391ad5b19ddb8a36a7a72
Maba KaloX <llocnex@gmail.com>
2021-04-02 04:28:34 +0300

    More intuitive image upload from frontend

--- frontend/src/Components/MyGallery/UploadButton/index.tsx
+++ frontend/src/Components/MyGallery/UploadButton/index.tsx
@@ -10,6 +10,7 @@ type UploadButtonProps = {
 
 enum UploadStateEnum {
     Initial,
+    FileSelected,
     Loading,
     Error,
     Success
@@ -18,9 +19,10 @@ enum UploadStateEnum {
 const GetIcon = (upload_state: UploadStateEnum) => {
     switch (upload_state) {
         case UploadStateEnum.Initial:
+        case UploadStateEnum.FileSelected:
             return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         className="bi bi-cloud-arrow-up" viewBox="0 0 16 16">
-                <path fill-rule="evenodd"
+                <path fillRule="evenodd"
                       d="M7.646 5.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 6.707V10.5a.5.5 0 0 1-1 0V6.707L6.354 7.854a.5.5 0 1 1-.708-.708l2-2z"/>
                 <path
                     d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
@@ -28,7 +30,7 @@ const GetIcon = (upload_state: UploadStateEnum) => {
         case UploadStateEnum.Loading:
             return <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         className="bi bi-arrow-clockwise loading" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
                 <path
                     d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
             </svg>
@@ -53,23 +55,24 @@ const GetIcon = (upload_state: UploadStateEnum) => {
 
 const get_button_v = (upload_state: UploadStateEnum) => {
     switch (upload_state) {
+        case UploadStateEnum.Initial:
+            return 'secondary'
+        case UploadStateEnum.FileSelected:
+        case UploadStateEnum.Loading:
+            return 'info'
         case UploadStateEnum.Error:
             return 'warning'
         case UploadStateEnum.Success:
             return 'success'
-        default:
-            return 'info'
     }
 }
 
 const UploadButton = (props: UploadButtonProps) => {
-    const [is_file_chosen, set_is_file_chosen] = useState(false)
     const [upload_state, set_upload_state] = useState<UploadStateEnum>(UploadStateEnum.Initial)
-
     const upload_input_element = useRef<HTMLInputElement>(null)
 
     const upload_file = () => {
-        if (upload_input_element && upload_input_element.current && is_file_chosen) {
+        if (upload_input_element && upload_input_element.current && UploadStateEnum.FileSelected) {
             if (
                 upload_input_element.current.files &&
                 upload_input_element.current.files.length === 1
@@ -84,6 +87,7 @@ const UploadButton = (props: UploadButtonProps) => {
                     }
                 }).then((response) => {
                     props.update_list_handler()
+                    console.log(response)
                     set_upload_state(UploadStateEnum.Success)
                 }).catch((error) => {
                     set_upload_state(UploadStateEnum.Error)
@@ -106,9 +110,9 @@ const UploadButton = (props: UploadButtonProps) => {
         set_upload_state(UploadStateEnum.Initial)
         if (upload_input_element && upload_input_element.current) {
             if (upload_input_element.current.files && upload_input_element.current.files.length === 1) {
-                set_is_file_chosen(true)
+                set_upload_state(UploadStateEnum.FileSelected)
             } else {
-                set_is_file_chosen(false)
+                set_upload_state(UploadStateEnum.Initial)
             }
         }
     }
@@ -118,7 +122,7 @@ const UploadButton = (props: UploadButtonProps) => {
             <ButtonGroup>
                 <label htmlFor="actual-upload-btn" className="my-0 btn btn-dark select_file_btn">Select File</label>
                 <Button onClick={upload_file} variant={get_button_v(upload_state)}
-                        disabled={!is_file_chosen}>
+                        disabled={upload_state == UploadStateEnum.Initial}>
                     {GetIcon(upload_state)}
                 </Button>
             </ButtonGroup>
